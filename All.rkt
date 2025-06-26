8ad45860a9359f2e3f3af7e74a8961095940e558
J-Weik <weikjustus1@gmail.com>
2025-06-26 02:11:22 +0200

    TODO: Add blossom toggle, fix change tests

--- All.rkt
+++ All.rkt
@@ -6,7 +6,7 @@
 ;; mit s lässt sich die Menge an branches die pro branch gemalt werden -1 setzen
 ;; mit q lässt sich die vorderste Farbe der FarbListe entfernen um das rendern zu beschleunigen
 ;; TIPP: BLOSSOM-SIZE auf 0 setzen um die Trees besser sehen zu können
-
+;; HINWEIS: Zugewiesener RAM für DrRacket muss möglicherweise hochgestellt werden
 
 ; Datendefinitionen
 
@@ -15,8 +15,8 @@
 
 ; Konstantendefinitionen
 
-(define BLOSSOM-SIZE 0)
-(define BLOSSOM-TYPE "outline")
+(define BLOSSOM-SIZE 5)
+(define BLOSSOM-TYPE "solid")
 (define TREE-CANVAS-SIZE-X 1000)
 (define TREE-CANVAS-SIZE-Y 1000)
 (define FIRST-BRANCH-LENGTH -150)
@@ -77,6 +77,7 @@
 ; Draws a line with length and direction given in vector in the color of color
 ; at the coordintes of posn into scene and returns scene
 
+; Tests sind in Abschnitt "weitere Ausdrücke"
 (define (put-branch pos vec color scene)
    (add-line scene
     (posn-x pos)(posn-y pos)
@@ -90,6 +91,7 @@
 ; posn color image -> image
 ; draws a circle in the color of color at position posn in the image and returns the image
 
+; Tests sind in Abschnitt "weitere Ausdrücke"
 (define (put-blossom pos color scene)
   (place-image
    (circle BLOSSOM-SIZE BLOSSOM-TYPE color)
@@ -164,6 +166,7 @@
 ; input -> WorldState
 ; takes input from keyboard and alters the worldState given from the input
 
+; Tests sind in Abschnitt "weitere Ausdrücke"
 (define (change world key)
   (cond
     [(key=? key "up") (make-WorldState
@@ -239,19 +242,37 @@
 (put-blossom (make-posn 43 340) "green"
  (put-blossom (make-posn 100 439) "green"
   (put-blossom (make-posn 250 400) "green"
-   (put-branch (make-posn 0 0) (make-vector (/ pi 2) 100) "green"
-    (put-branch (make-posn 250 250) (make-vector (/ pi 4) 150) "green"
-     (put-branch (make-posn 250 250) (make-vector (* pi (/ 3 2)) 200) "green"
-      (put-image (circle BLOSSOM-SIZE BLOSSOM-TYPE "red") 43 340
-       (put-image (circle BLOSSOM-SIZE BLOSSOM-TYPE "red") 100 439
-        (put-image (circle BLOSSOM-SIZE BLOSSOM-TYPE "red") 250 400
-         (add-line (add-line (add-line (empty-scene 500 500)1 1 1 1 "red")250 250 325 325 "red") 250 250 250 50 "red"))))))))))
+   (put-branch (make-posn 0 0) (make-vector (/ pi 3) 100) "green"
+    (put-branch (make-posn 250 250) (make-vector (/ pi 4) 106) "green"
+     (put-branch (make-posn 250 250) (make-vector (* 1.5 pi) 200) "green"
+      (place-image (circle BLOSSOM-SIZE BLOSSOM-TYPE "red") 43 340
+       (place-image (circle BLOSSOM-SIZE BLOSSOM-TYPE "red") 100 439
+        (place-image (circle BLOSSOM-SIZE BLOSSOM-TYPE "red") 250 400
+         (add-line
+          (add-line
+           (add-line (empty-scene 500 500)
+                     0 0
+                     (posn-x (polar->cartesian (/ pi 3) 100))
+                     (posn-y (polar->cartesian (/ pi 3) 100))
+                     "red")
+           250 250
+           (+ 250 (posn-x (polar->cartesian (/ pi 4) 106)))
+           (+ 250 (posn-y (polar->cartesian (/ pi 4) 106)))
+           "red")
+          250 250
+          (+ 250 (posn-x (polar->cartesian (* 1.5 pi) 200)))
+          (+ 250 (posn-y (polar->cartesian (* 1.5 pi) 200)))
+          "red"))))))))))
 
 ; Tests für change
 
 (define CHANGE_TEST1 (change (change (change DEFAULT_WORLD_STATE "up") "left") "+"))
 (define CHANGE_TEST2 (change (change (change DEFAULT_WORLD_STATE "down") "right") "-"))
+(define CHANGE_TEST3 (change DEFAULT_WORLD_STATE "w"))
+(define CHANGE_TEST4 (change DEFAULT_WORLD_STATE "s"))
+(define CHANGE_TEST5 (change DEFAULT_WORLD_STATE "q"))
 
+; Tests for CHANGE_TEST1
 (check-within (WorldState-verzweigung CHANGE_TEST1) (+ (/ pi 3) 0.1) 1e-6)
 (check-within (WorldState-growth CHANGE_TEST1) 0.76 1e-6)
 (check-expect (WorldState-colorList CHANGE_TEST1)
@@ -267,6 +288,7 @@
                 ,(make-color 255 0 255)
                 ))
 
+; Tests for CHANGE_TEST2
 (check-expect (WorldState-branches CHANGE_TEST1) 2)
 (check-within (WorldState-verzweigung CHANGE_TEST2) (- (/ pi 3) 0.1) 1e-6)
 (check-within (WorldState-growth CHANGE_TEST2) 0.56 1e-6)
@@ -283,3 +305,53 @@
                 ,(make-color 255 0 255)
                 ))
 (check-expect (WorldState-branches CHANGE_TEST2) 2)
+
+; Tests for CHANGE_TEST3
+(check-within (WorldState-verzweigung CHANGE_TEST3) (/ pi 3) 1e-6)
+(check-within (WorldState-growth CHANGE_TEST3) 0.66 1e-6)
+(check-expect (WorldState-colorList CHANGE_TEST3)
+              `(
+                ,(make-color 255 0 255)
+                ,(make-color 255 0 64)
+                ,(make-color 255 0 0)
+                ,(make-color 255 128 0)
+                ,(make-color 128 255 0)
+                ,(make-color 0 255 128)
+                ,(make-color 0 255 255)
+                ,(make-color 0 128 255)
+                ,(make-color 64 0 255)
+                ))
+(check-expect (WorldState-branches CHANGE_TEST3) 3)
+
+; Tests for CHANGE_TEST4
+(check-within (WorldState-verzweigung CHANGE_TEST4) (/ pi 3) 1e-6)
+(check-within (WorldState-growth CHANGE_TEST4) 0.66 1e-6)
+(check-expect (WorldState-colorList CHANGE_TEST4)
+              `(
+                ,(make-color 255 0 255)
+                ,(make-color 255 0 64)
+                ,(make-color 255 0 0)
+                ,(make-color 255 128 0)
+                ,(make-color 128 255 0)
+                ,(make-color 0 255 128)
+                ,(make-color 0 255 255)
+                ,(make-color 0 128 255)
+                ,(make-color 64 0 255)
+                ))
+(check-expect (WorldState-branches CHANGE_TEST4) 1)
+
+; Tests for CHANGE_TEST5
+(check-within (WorldState-verzweigung CHANGE_TEST5) (/ pi 3) 1e-6)
+(check-within (WorldState-growth CHANGE_TEST5) 0.66 1e-6)
+(check-expect (WorldState-colorList CHANGE_TEST5)
+              `(
+                ,(make-color 255 0 64)
+                ,(make-color 255 0 0)
+                ,(make-color 255 128 0)
+                ,(make-color 128 255 0)
+                ,(make-color 0 255 128)
+                ,(make-color 0 255 255)
+                ,(make-color 0 128 255)
+                ,(make-color 64 0 255)
+                ))
+(check-expect (WorldState-branches CHANGE_TEST5) 2)
